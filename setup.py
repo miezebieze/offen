283a4e9c5701ce2cdb568133c05d4dc3faa807f0
mieze :3 <mieze@miezys.de>
2020-01-19 00:10:25 +0100

    whyever I need to make another commit ...

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from distutils.core import setup
 import offen
 
 setup (name = 'open-fenfen',
-       #version = ftl.__version__,
+       #version = offen.__version__,
        description = 'Framework for interactive fiction and text adventures.',
-       #long_description = ftl.__doc__,
-       #author = ftl.__author__,
-       #author_email = ftl.__email__,
+       #long_description = offen.__doc__,
+       #author = offen.__author__,
+       #author_email = offen.__email__,
        url = 'github.com/miezebieze/offen',
        packages = ['offen'],
        license = 'MIT',
